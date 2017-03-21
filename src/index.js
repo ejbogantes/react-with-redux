cda2de96921e5623c74bb264e71be215d662c70a
Emilio Bogantes <ejbogantes@gmail.com>
2017-03-21 09:53:13 -0600

    Cambio de estructura

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+import './assets/css/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render( < App / > ,
+    document.getElementById('root')
 );
